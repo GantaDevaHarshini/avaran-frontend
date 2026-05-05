f79d8cab7dfdd494201a18367dfae548c75323b2
GantaDevaHarshini <devaharshiniganta@gmail.com>
2026-05-05 18:28:55 +0530

    Changes in the axios

--- src/api/client.js
+++ src/api/client.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const api = axios.post("https://your-render-url/auth/register")({
+const api = axios.create({
   baseURL: process.env.REACT_APP_API_URL || "https://avaran-backend.onrender.com",
   headers: {
     "Content-Type": "application/json",
